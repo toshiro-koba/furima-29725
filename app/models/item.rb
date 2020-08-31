e52da28ed231406418bde84383c57f102c54b4fb
toshiro-koba <kobato.231.0625@gmail.com>
2020-08-31 21:38:07 +0900

    アソシエーションとバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+  belongs_to_active_hash :category, :status, :cover_expenses, :area, :delivery_time
+
+  validates :item_name, :description, :category, :status, :cover_expenses, :area, :delivery_time, :price, presence: true
+  #リファレンス型のuserも入れる？
+
+  validates :category_id, :status_id, :cover_expenses_id, :area_id, :delivery_time_id, numericality: { other_than: 1}
+
+
 end
