ffb44eff5195556984756104f321485c0b88c94c
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-02 14:03:28 +0900

    価格のバリデーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :delivery_time
 
-  validates :image, :item_name, :description, :category, :status, :cover_expense, :area, :delivery_time, presence: true
+  validates :image, :item_name, :description, :category, :status, :cover_expense, :area, :delivery_time, :price, presence: true
 
   validates :category_id, :status_id, :cover_expense_id, :area_id, :delivery_time_id, numericality: { other_than: 1, message: 'Select' }
 
-  with_options presence: true, format: { with: /\d{}/, message: 'Half-width number' }, numericality: { only_integer: true, greater_than_or_equal_to: 300, message: 'Out of setting range', less_than_or_equal_to: 9_999_999 } do
-    validates :price
-  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, message: 'Out of setting range', less_than_or_equal_to: 9_999_999 }
+
+  validates :price, numericality: { only_integer: true, message: "Half-width number" }
 end
