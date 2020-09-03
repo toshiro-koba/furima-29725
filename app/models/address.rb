c3f939e6802db389d1998b6094b6a31c00a69a46
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-03 19:40:14 +0900

    バリデーションを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :sold_item
 
-  validates :prefectures, :city, :address, :presence, true
-  validates :phone_number, numericality: { only_integer: true }
-  # 電話番号の桁数もバリデーションする！！
+  validates :prefectures, :city, :address, :phone_number, presence: true
+  validates :phone_number, length: { maximum: 11 }
   validates :prefectures_id, numericality: { other_than: 1, message: 'Select' }
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
   end
 end
+
 
