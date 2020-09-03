11fb689d503ae0d2b76c4a24698374b78243b2bc
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-03 19:20:39 +0900

    アソシエーションを記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ApplicationRecord
+  belongs_to :sold_item
+
+  validates :prefectures, :city, :address, :presence, true
+  validates :phone_number, numericality: { only_integer: true }
+  # 電話番号の桁数もバリデーションする！！
+  validates :prefectures_id, numericality: { other_than: 1, message: 'Select' }
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  end
 end
+
