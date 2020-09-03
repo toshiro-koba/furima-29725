6065e152b70a0a10eb75e35bc0d6890034c40904
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-03 16:46:25 +0900

    sold_itemsコントローラの作成

---
+++ app/controllers/sold_items_controller.rb
@@ -0,0 +1,33 @@
+class SoldItemsController < ApplicationController
+end
+class SoldItemsController < ApplicationController
+  def index
+    @sold_item = SoldItem.new(sold_item_params)
+    @item =Item.find(params[:item_id])
+    
+  end
+
+  def create
+    if @sold_item.valid?
+      pay_item
+      @sold_item.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+  private
+
+  def sold_item_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_5b5837b1f44d0df569331e9d"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: sold_item_params[:price],  # 商品の値段
+      card: sold_item_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+end
