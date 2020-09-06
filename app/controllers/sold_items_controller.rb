7ac8826239770a6d8e5a6a421a6db6151afd2fc2
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-06 19:15:02 +0900

    herokuでトークン生成できなかったので、pk,skを新規発行

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -29,11 +29,11 @@ class SoldItemsController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: @item[:price], # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy'                 # 通貨の種類(日本円)
+      amount: @item[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
     )
   end
 
